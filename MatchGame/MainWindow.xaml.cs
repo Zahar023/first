d618e8c149f338c097e4b455afed1224b5a4ad4e
Zahar023 <77553264+Zahar023@users.noreply.github.com>
2024-04-27 09:07:17 +0700

    Add files via upload

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    using System.Windows.Threading;
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private int _tenthsOfSecondsElapsed ;
+        private int _matchesFound;
+        private TextBlock _lastTextBlockClicked;
+        private bool _findingMatch = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _timer.Interval = TimeSpan.FromSeconds(.1);
+            _timer.Tick += Timer_Tick;
+            SetUpGame();
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, EventArgs eventArgs)
+        {
+            if (_matchesFound == 8)
+            {
+                SetUpGame();
+            }
+        }
+
+        
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _tenthsOfSecondsElapsed++;
+            TimerTextBlock.Text = (_tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (_matchesFound == 8)
+            {
+                _timer.Stop();
+                TimerTextBlock.Text += "Play Again?";
+            }
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐵", "🐵",
+                "🐶", "🐶",
+                "🐱", "🐱",
+                "🦝", "🦝",
+                "🐮", "🐮",
+                "🐷", "🐷",
+                "🦒", "🦒",
+                "🐻", "🐻",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in MainGrid.Children.OfType<TextBlock>())
+            {
+                if (animalEmoji.Count != 0)
+                {
+                    int index = random.Next(animalEmoji.Count);
+                    string animalText = animalEmoji[index];
+                    textBlock.Text = animalText;
+                    textBlock.Visibility = Visibility.Visible;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+            _timer.Start();
+            _tenthsOfSecondsElapsed = 0;
+            _matchesFound = 0;
+        }
+
+        public void TextBlock_MouseDown(object sender, MouseButtonEventArgs eventArgs)
+        {
+            TextBlock textBlock = (TextBlock)sender;
+            if (_findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                _lastTextBlockClicked = textBlock;
+                _findingMatch = true;
+            } 
+            else if (_lastTextBlockClicked.Text == textBlock.Text)
+            {
+                _matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                _findingMatch = false;
+            }
+            else
+            {
+                _lastTextBlockClicked.Visibility = Visibility.Visible;
+                _findingMatch = false;
+            }
+        }
+    }
+}
---
+++ Pong/Pong.cpp
@@ -0,0 +1,257 @@
+﻿
+
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+
+#define HEIGHT = 25
+#define WIDTH = 80
+
+void output(int** matrix, int n, int m);
+void read_numbers(FILE* input, int** numbers);
+void check_elements(int** matrix, int** matrix2);
+void rewrite(int** matrix, int** matrix2);
+void left_top_angle(int* array, int** matrix, int i, int j);
+void right_top_angle(int* array, int** matrix, int i, int j);
+void left_bottom_angle(int* array, int** matrix, int i, int j);
+void right_bottom_angle(int* array, int** matrix, int i, int j);
+void top(int* array, int** matrix, int i, int j);
+void bottom(int* array, int** matrix, int i, int j);
+void right(int* array, int** matrix, int i, int j);
+void left(int* array, int** matrix, int i, int j);
+void center(int* array, int** matrix, int i, int j);
+
+int main() {
+    int nx = 25, ny = 80, time = 0;
+
+    FILE* input = fopen("file2.txt", "r");
+
+    int** array1 = (int**)malloc(nx * sizeof(int*));
+    for (int i = 0; i < nx; i++) {
+        array1[i] = (int*)malloc(ny * sizeof(int));
+    }
+    int** array2 = (int**)malloc(nx * sizeof(int*));
+    for (int i = 0; i < nx; i++) {
+        array2[i] = (int*)malloc(ny * sizeof(int));
+    }
+    read_numbers(input, array1);
+    output(array1, nx, ny);
+    while (1) {
+        time++;
+        if (time == 20000000) {
+            printf("\033[0d\033[2J");
+            check_elements(array1, array2);
+            output(array1, nx, ny);
+            rewrite(array1, array2);
+            printf("\n");
+            time = 0;
+        }
+    }
+    for (int i = 0; i < nx; i++) {
+        free(array1[i]);
+    }
+    free(array1);
+    for (int i = 0; i < nx; i++) {
+        free(array2[i]);
+    }
+    free(array2);
+
+    return 0;
+}
+
+void rewrite(int** matrix, int** matrix2) {
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 80; j++) {
+            matrix[i][j] = matrix2[i][j];
+        }
+    }
+}
+
+void check_elements(int** matrix, int** matrix2) {
+    int* array = (int*)malloc(9 * sizeof(int));
+    int count = 0;
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 80; j++) {
+            if ((i == 0) && (j == 0)) {
+                left_top_angle(array, matrix, i, j);
+            }
+            else if ((i == 0) && (j == 79)) {
+                right_top_angle(array, matrix, i, j);
+            }
+            else if (i == 0) {
+                top(array, matrix, i, j);
+            }
+            else if ((i == 24) && (j == 0)) {
+                left_bottom_angle(array, matrix, i, j);
+            }
+            else if ((i == 24) && (j == 79)) {
+                right_bottom_angle(array, matrix, i, j);
+            }
+            else if (i == 24) {
+                bottom(array, matrix, i, j);
+            }
+            else if (j == 0) {
+                left(array, matrix, i, j);
+            }
+            else if (j == 79) {
+                right(array, matrix, i, j);
+            }
+            else {
+                center(array, matrix, i, j);
+            }
+            for (int l = 1; l < 9; l++) {
+                if (array[l] == 1) {
+                    count++;
+                }
+            }
+            if ((array[0] == 0) && (count == 3)) {
+                matrix2[i][j] = 1;
+            }
+            else if ((array[0] == 1) && ((count == 2) || count == 3)) {
+                matrix2[i][j] = 1;
+            }
+            else if ((array[0] == 1)) {
+                matrix2[i][j] = 0;
+            }
+            else if ((array[0] == 0)) {
+                matrix2[i][j] = 0;
+            }
+            count = 0;
+        }
+    }
+    free(array);
+}
+
+void left_top_angle(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][j + 1];
+    array[2] = matrix[i][79];
+    array[3] = matrix[i + 1][j];
+    array[6] = matrix[i + 1][j + 1];
+    array[4] = matrix[24][j + 1];
+    array[5] = matrix[24][j];
+    array[7] = matrix[24][79];
+    array[8] = matrix[i + 1][79];
+}
+void right_top_angle(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][0];
+    array[2] = matrix[i][j - 1];
+    array[3] = matrix[i + 1][j];
+    array[6] = matrix[i + 1][0];
+    array[4] = matrix[24][0];
+    array[5] = matrix[24][j];
+    array[7] = matrix[24][j - 1];
+    array[8] = matrix[i + 1][j - 1];
+}
+void left_bottom_angle(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][j + 1];
+    array[2] = matrix[i][79];
+    array[3] = matrix[0][j];
+    array[6] = matrix[0][j + 1];
+    array[4] = matrix[i - 1][j + 1];
+    array[5] = matrix[i - 1][j];
+    array[7] = matrix[i - 1][79];
+    array[8] = matrix[0][79];
+}
+void right_bottom_angle(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][0];
+    array[2] = matrix[i][j - 1];
+    array[3] = matrix[0][j];
+    array[6] = matrix[0][0];
+    array[4] = matrix[i - 1][0];
+    array[5] = matrix[i - 1][j];
+    array[7] = matrix[i - 1][j - 1];
+    array[8] = matrix[0][j - 1];
+}
+
+void top(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][j + 1];
+    array[2] = matrix[i][j - 1];
+    array[3] = matrix[i + 1][j];
+    array[6] = matrix[i + 1][j + 1];
+    array[4] = matrix[24][j + 1];
+    array[5] = matrix[24][j];
+    array[7] = matrix[24][j - 1];
+    array[8] = matrix[i + 1][j - 1];
+}
+void bottom(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][j + 1];
+    array[2] = matrix[i][j - 1];
+    array[3] = matrix[0][j];
+    array[6] = matrix[0][j + 1];
+    array[4] = matrix[i - 1][j + 1];
+    array[5] = matrix[i - 1][j];
+    array[7] = matrix[i - 1][j - 1];
+    array[8] = matrix[0][j - 1];
+}
+void left(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][j + 1];
+    array[2] = matrix[i][79];
+    array[3] = matrix[i + 1][j];
+    array[6] = matrix[i + 1][j + 1];
+    array[4] = matrix[i - 1][j + 1];
+    array[5] = matrix[i - 1][j];
+    array[7] = matrix[i - 1][79];
+    array[8] = matrix[i + 1][79];
+}
+void right(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][0];
+    array[2] = matrix[i][j - 1];
+    array[3] = matrix[i + 1][j];
+    array[6] = matrix[i + 1][0];
+    array[4] = matrix[i - 1][0];
+    array[5] = matrix[i - 1][j];
+    array[7] = matrix[i - 1][j - 1];
+    array[8] = matrix[i + 1][j - 1];
+}
+
+void center(int* array, int** matrix, int i, int j) {
+    array[0] = matrix[i][j];
+    array[1] = matrix[i][j + 1];
+    array[2] = matrix[i][j - 1];
+    array[3] = matrix[i + 1][j];
+    array[6] = matrix[i + 1][j + 1];
+    array[4] = matrix[i - 1][j + 1];
+    array[5] = matrix[i - 1][j];
+    array[7] = matrix[i - 1][j - 1];
+    array[8] = matrix[i + 1][j - 1];
+}
+
+void read_numbers(FILE* input, int** numbers) {
+    fseek(input, 0, SEEK_SET);
+    for (int i = 0; i < 25; ++i) {
+        for (int j = 0; j < 80; ++j) {
+            fscanf(input, "%d", &numbers[i][j]);
+        }
+    }
+}
+
+void output(int** matrix, int lenx, int leny) {
+    for (int i = 0; i < lenx; i++) {
+        for (int j = 0; j < leny; j++) {
+            if (j == 0) {
+                if (matrix[i][j] == 0)
+                    printf(" ");
+                else
+                    printf("%d", matrix[i][j]);
+
+            }
+            else {
+                if (matrix[i][j] == 0)
+                    printf(" ");
+                else
+                    printf("%d", matrix[i][j]);
+            }
+        }
+        if ((i + 1) < lenx) {
+            printf("\n");
+        }
+    }
+}
